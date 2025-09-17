ff206811e95b6dcea434f4856dce1ec3619e9029
Chloe Nakamanya <nakamanyachloe4@gmail.com>
2025-09-17 18:30:48 +0200

    "Skapat FormatName metod"

--- Program.cs
+++ Program.cs
@@ -4,9 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(FormatName("Mary", "Jacksson"));
+            Console.WriteLine(FormatName("Mary", "Jane", "Jacksson"));
+            Console.WriteLine(FormatName("Mary", "Jacksson", true));
+
         }
-  
+
+        static string FormatName(string firstName, string lastName)
+        {
+            return $"{firstName} {lastName}";
+        }
+   
+    static string FormatName(string firstName, string middle, string lastName)
+        {
+            return $"{firstName} {middle} {lastName}";
+        }    
+   
+    static string FormatName(string firstName, string lastName, bool uppercase)
+        {
+            var full = $"{firstName} {lastName}";
+            return uppercase ? full.ToUpper() : full ;
+        }    
     
     }
 }
